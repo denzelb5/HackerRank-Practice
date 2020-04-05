98e272a08428e4e1398dc076d92fab49bf0ccfaa
denise baker <denviol@yahoo.com>
2020-04-04 20:53:33 -0500

    Add project files.

---
+++ HackerRank-Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HackerRank_Practice
+{
+    class Program
+    {
+        static void solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+
+            var tax = meal_cost * (tax_percent * .01);
+            var tip = meal_cost * (tip_percent * .01);
+            var total = meal_cost + tax + tip;
+            var rounded = Math.Round(total);
+            Console.WriteLine(rounded);
+
+        }
+
+        static void Main(string[] args)
+        {
+            solve(12.00, 20, 8);
+        }
+    }
+}
